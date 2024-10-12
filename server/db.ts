b9d5ec67bb751ae73cdda17f41e692bdf8419313
Juan <juancruzmur@gmail.com>
2024-10-12 11:04:05 -0300

    test service account nuevo

--- server/db.ts
+++ server/db.ts
@@ -1,20 +1,3 @@
-//import admin from "firebase-admin";
-/*
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import dotenv from "dotenv";
-import { getFirestore } from "firebase/firestore";
-dotenv.config();
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG!);
-const databaseURL = process.env.DATABASE_URL.toString();
-
-const app = initializeApp(firebaseConfig);
-
-const fireStore = getFirestore(app);
-const rtdb = getDatabase(app);
-
-export { fireStore, rtdb, app };*/
-
 import admin from "firebase-admin";
 import dotenv from "dotenv";
 
@@ -23,24 +6,14 @@ const databaseURL =
   process.env.DATABASE_URL ||
   "https://desafio-nivel2-final-default-rtdb.firebaseio.com";
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_KEY_ID,
-  private_key:
-    process.env.NODE_ENV === "production"
-      ? process.env.FIREBASE_PRIVATE_KEY
-      : process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_MAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_CERT,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+const serviceAccount: admin.ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_MAIL!,
 };
 
 export const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: databaseURL,
 });
 
