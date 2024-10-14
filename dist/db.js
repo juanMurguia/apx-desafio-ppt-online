fd1739d2570529a0b0b6557b4f433f2f4cbd295d
Juan <juancruzmur@gmail.com>
2024-10-13 23:18:24 -0300

    uso json como service account

--- dist/db.js
+++ dist/db.js
@@ -1,24 +1,9 @@
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-import serviceAccount from "../desafio-nivel2-final-firebase-adminsdk-4wvjk-72c628992d.json" assert { type: "json" };
+import serviceAccount from "../desafio-nivel2-final-firebase-adminsdk-4wvjk-37c46c20bf.json" assert { type: "json" };
 dotenv.config();
 const databaseURL = process.env.DATABASE_URL ||
     "https://desafio-nivel2-final-default-rtdb.firebaseio.com";
-/*const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") // Reemplaza \n por saltos de línea reales
-    : undefined,
-  client_email: process.env.FIREBASE_CLIENT_MAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_CERT,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
-};*/
-console.log(serviceAccount);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.DATABASE_URL,
--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,12 @@
 import admin, { ServiceAccount } from "firebase-admin";
 import dotenv from "dotenv";
-import serviceAccount from "../desafio-nivel2-final-firebase-adminsdk-4wvjk-72c628992d.json" assert { type: "json" };
+import serviceAccount from "../desafio-nivel2-final-firebase-adminsdk-4wvjk-37c46c20bf.json" assert { type: "json" };
+
 dotenv.config();
 const databaseURL =
   process.env.DATABASE_URL ||
   "https://desafio-nivel2-final-default-rtdb.firebaseio.com";
 
-console.log(serviceAccount);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
   databaseURL: process.env.DATABASE_URL,
